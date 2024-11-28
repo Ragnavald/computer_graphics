453e4aee54fd5f938d2b72ccc05bffb86f5703be
Ronaldo Ávila <ronaldo.avila@picolotec.com.br>
2024-11-28 14:48:15 -0300

    feat: replace cube to cylinder im arm and forearm

--- main.cpp
+++ main.cpp
@@ -2,6 +2,14 @@
 
 float baseRotate = 0.0f, armRotate = 0.0f, forearmRotate = 0.0f, clawRotate = 0.0f;
 
+float armSizeHeight = 1.0f;
+
+float armBaseRadiusSize = 0.165f;
+float baseBaseRadiusSize = 0.23f;
+
+float forearmBaseRadiusSize = 0.12f;
+float forearmSizeHeight = 1.2f;
+
 void init()
 {
     glEnable(GL_DEPTH_TEST);
@@ -12,40 +20,86 @@ void init()
     glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
 }
 
+void drawAxes()
+{
+    glLineWidth(2.0); // Aumenta a espessura das linhas
+    glBegin(GL_LINES);
+
+    // Eixo X (Vermelho)
+    glColor3f(1.0, 0.0, 0.0);
+    glVertex3f(-2.0, 0.0, 0.0);
+    glVertex3f(2.0, 0.0, 0.0);
+
+    // Eixo Y (Verde)
+    glColor3f(0.0, 1.0, 0.0);
+    glVertex3f(0.0, -2.0, 0.0);
+    glVertex3f(0.0, 2.0, 0.0);
+
+    // Eixo Z (Azul)
+    glColor3f(0.0, 0.0, 1.0);
+    glVertex3f(0.0, 0.0, -2.0);
+    glVertex3f(0.0, 0.0, 2.0);
+
+    glEnd();
+}
+
+void drawCylinder(float baseRadius, float topRadius, float height, float angle, float x, float y, float z)
+{
+    GLUquadric *quad = gluNewQuadric();
+    glPushMatrix();
+    glRotatef(angle, x, y, z); // Rotação no ângulo e eixo especificados
+    gluCylinder(quad, baseRadius, topRadius, height, 20, 20);
+    glPopMatrix();
+
+    gluDeleteQuadric(quad);
+}
+
 void drawBase()
 {
     glColor3f(0.5f, 0.35f, 0.05f);
-
-    // Desenhar o topo do cilindro como um disco
-    glPushMatrix();
-    glutSolidCone(0.2, 0.2, 20, 1);
-    glPopMatrix();
-
-    // Desenhar o corpo do cilindro como um cone com zero de altura
+    // Desenhar a esfera
     glPushMatrix();
     glTranslatef(0.0f, 0.1f, 0.0f);
-    glRotatef(180, 1.0, 0.0, 0.0); // Rotacionar o cone para ficar de cabeça para baixo
-    glutSolidCone(0.2, 0.1, 20, 20);
+    glutSolidSphere(baseBaseRadiusSize, baseBaseRadiusSize *100, baseBaseRadiusSize*100); // Raio = 0.2, subdivisões longitude/latitude = 20
     glPopMatrix();
 }
+
+void drawArmArticulation()
+{
+    glColor3f(0.5f, 0.35f, 0.05f);
+    // Desenhar a esfera
+    glPushMatrix();
+    glTranslatef(0, armSizeHeight, 0.0f);
+    glutSolidSphere(armBaseRadiusSize, armBaseRadiusSize *100, armBaseRadiusSize *100); // Raio = 0.2, subdivisões longitude/latitude = 20
+    glPopMatrix();
+}
+
+void drawHandForearmArticulation()
+{
+    glColor3f(0.5f, 0.35f, 0.05f);
+    // Desenhar a esfera
+    glPushMatrix();
+    glTranslatef(0, forearmSizeHeight, 0.0f);
+    glutSolidSphere(forearmBaseRadiusSize, forearmBaseRadiusSize *100, forearmBaseRadiusSize *100); // Raio = 0.2, subdivisões longitude/latitude = 20
+    glPopMatrix();
+}
+
 
 void drawArm()
 {
     glColor3f(0.8f, 0.5f, 0.2f);
     glPushMatrix();
-    glTranslatef(0.0f, 0.5f, 0.0f);
-    glScalef(0.2f, 1.0f, 0.2f);
-    glutSolidCube(1.0);
+    glTranslatef(0.0f, 1.0f, 0.0f);
+    drawCylinder(armBaseRadiusSize, baseBaseRadiusSize, armSizeHeight,90.f,0.1f,0.0f,0.0f);
     glPopMatrix();
 }
 
 void drawForearm()
 {
-    glColor3f(0.6f, 0.4f, 0.2f);
+    glColor3f(0.8f, 0.5f, 0.2f);
     glPushMatrix();
-    glTranslatef(0.0f, 0.5f, 0.0f);
-    glScalef(0.15f, 1.0f, 0.15f);
-    glutSolidCube(1.0);
+    glTranslatef(0.0f, forearmSizeHeight, 0.0f);
+    drawCylinder(forearmBaseRadiusSize, armBaseRadiusSize, forearmSizeHeight,90.f,0.1f,0.0f,0.0f);
     glPopMatrix();
 }
 
@@ -53,8 +107,8 @@ void drawClaw()
 {
     glColor3f(0.4f, 0.3f, 0.2f);
     glPushMatrix();
-    glTranslatef(0.0f, 0.25f, 0.0f);
-    glScalef(0.1f, 0.5f, 0.1f);
+    glTranslatef(0.0f, 0.5f, 0.0f);
+    glScalef(0.14f, 0.5f, 0.3f);
     glutSolidCube(1.0);
     glPopMatrix();
 }
@@ -65,6 +119,7 @@ void display()
     glLoadIdentity();
     gluLookAt(2.0, 2.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 
+    drawAxes();
     // Base rotation
     glPushMatrix();
     glRotatef(baseRotate, 0.0, 1.0, 0.0);
@@ -75,10 +130,14 @@ void display()
     glRotatef(armRotate, 0.0, 0.0, 1.0);
     drawArm();
 
+    drawArmArticulation();
+
     // Forearm rotation
     glTranslatef(0.0, 1.0, 0.0);
     glRotatef(forearmRotate, 0.0, 0.0, 1.0);
     drawForearm();
+
+    drawHandForearmArticulation();
 
     // Claw rotation
     glTranslatef(0.0, 1.0, 0.0);
